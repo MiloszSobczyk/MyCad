3ed8428ed3dee1bdb3b44000c0b45e014d06dcce
MiloszSobczyk <m.sobczyk.official@gmail.com>
2025-03-17 13:42:27 +0100

    Refactor IndexBuffer

--- MyCad/Source/Engine/IndexBuffer.cpp
+++ MyCad/Source/Engine/IndexBuffer.cpp
@@ -14,17 +14,17 @@ IndexBuffer::IndexBuffer(std::vector<unsigned int> indices)
 
 IndexBuffer::~IndexBuffer()
 {
-	GLCall(glDeleteBuffers(1, &id));
+	glDeleteBuffers(1, &id);
 }
 
-unsigned int IndexBuffer::GetCount() const
+int IndexBuffer::GetCount() const
 {
 	return count;
 }
 
 void IndexBuffer::SetIndices(std::vector<unsigned int> indices)
 {
-	count = static_cast<unsigned int>(indices.size());
+	count = indices.size();
 	Bind();
 	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_DYNAMIC_DRAW));
 	Unbind();
--- MyCad/Source/Engine/IndexBuffer.h
+++ MyCad/Source/Engine/IndexBuffer.h
@@ -1,21 +1,24 @@
 #pragma once
-#include "Utils/Logger.h"
+
 #include <vector>
+
+#include "utils/GlCall.h"
 
 class IndexBuffer
 {
 private:
-	unsigned int count;
 	unsigned int id;
-
+	int count;
 public:
 	IndexBuffer();
 	IndexBuffer(std::vector<unsigned int> indices);
 	~IndexBuffer();
 
+	int GetCount() const;
+
+	void SetIndices(std::vector<unsigned int> indices);
+	
 	void Bind() const;
 	void Unbind() const;
-
-	unsigned int GetCount() const;
-	void SetIndices(std::vector<unsigned int> indices);
 };
+
