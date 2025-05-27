dd474e2602dff27d5b341e11d0ed88b697183301
MiloszSobczyk <m.sobczyk.official@gmail.com>
2025-05-27 23:18:23 +0200

    Add IdManager class

---
+++ MyCad/Source/Managers/IdManager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+
+class Shape;
+
+class IdManager
+{
+private:
+    static unsigned int currentId;
+    static std::vector<std::weak_ptr<Shape>> shapes;
+
+public:
+    static unsigned int GetNextId();
+    static std::vector<std::shared_ptr<Shape>> GetShapes();
+
+    static void RegisterShape(const std::shared_ptr<Shape>& shape);
+    static void UnregisterShape(const std::shared_ptr<Shape>& shape);
+};
