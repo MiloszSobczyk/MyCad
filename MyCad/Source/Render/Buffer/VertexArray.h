498f7114e52a4cc1e0a6c35736c5598fa12cf168
MiloszSobczyk <m.sobczyk.official@gmail.com>
2025-07-19 15:42:44 +0200

    Add VertexArray class

---
+++ MyCad/Source/Render/Buffer/VertexArray.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "VertexBuffer.h"
+#include "IndexBuffer.h"
+#include "Utils/Base.h"
+
+#include <vector>
+#include <memory>
+#include <cstdint>
+
+class VertexArray
+{
+public:
+	VertexArray();
+	~VertexArray();
+
+	void Bind() const;
+	void Unbind() const;
+
+	void AddVertexBuffer(const Ref<VertexBuffer>& vertexBuffer);
+	void SetIndexBuffer(const Ref<IndexBuffer>& indexBuffer);
+
+	const std::vector<Ref<VertexBuffer>>& GetVertexBuffers() const { return m_VertexBuffers; }
+	const Ref<IndexBuffer>& GetIndexBuffer() const { return m_IndexBuffer; }
+
+private:
+	uint32_t m_RendererID = 0;
+	uint32_t m_VertexBufferIndex = 0;
+	std::vector<Ref<VertexBuffer>> m_VertexBuffers;
+	Ref<IndexBuffer> m_IndexBuffer;
+};
